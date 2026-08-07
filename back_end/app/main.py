2f6768a5302f8a894c73ec5ba6525c677fdabe14
hemanth174 <ramasaiahemanth@gmail.com>
2026-08-07 14:06:27 +0530

    small change

--- back_end/app/main.py
+++ back_end/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="CodeAtlas API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
+        "http://localhost:5174",
     ],
     allow_credentials=True,
     allow_methods=["*"],
